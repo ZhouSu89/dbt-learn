96f29e4613584be66b9f65f96a915a844aadcfad
Zhou Su <suzhouhust@gmail.com>
2021-08-28 15:20:27 +0000

    practice models

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,13 @@
+with payments as (
+    select
+        orderid as order_id,
+        id as customer_id,
+        paymentmethod as payment_method,
+        status,
+        amount/100 as amount,
+        created as order_date
+    from  `dbt-tutorial.stripe.payment`   
+    -- where status = 'success'
+)
+
+select * from payments
